696c3d5c8765d2eba3d6f9e729efd49706fd727e
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-07-30 11:22:12 +0900

    add App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React, { useState, useEffect } from "react";
+import Router from "./routes/Router";
+import styled from "styled-components";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="style-app">
+      <Router />
+    </div>
+  );
 }
 
 export default App;
